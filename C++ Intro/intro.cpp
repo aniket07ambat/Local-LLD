79a963ac55cee453694ee68eece8895c14066d7a
aniket07ambat <ambataniket07gmail.com>
2024-01-26 17:06:35 +0530

    code practice

---
+++ C++ Intro/intro.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Homework Answers.\n";
+
+    bool abc;
+    cin >> abc;
+    cout << "When you input bool give 0 or 1 not true and false. " << endl;
+    cout << abc << endl;
+    return 0;
+}
+// std::cout and #include preprocessor directive.
+// https://chat.openai.com/share/be9cd68c-0f40-4dff-b312-f995531e4c4c
+
+// high level and low level programming difference.
+// https://chat.openai.com/share/0cea5aa1-14ba-4b79-90eb-f42c8a9c49a9
+
+// ::  => meaning?
